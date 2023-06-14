e43c07341a90f747e94bfafa721a0c4b4c5ee608
Jamie Phillips <cphill11@gmail.com>
2023-06-13 23:11:31 -0400

    Basic setup of the repository

---
+++ argo-dotnet-terraform-azure.build.ps1
@@ -0,0 +1,22 @@
+<#
+.Synopsis
+	Build script, https://github.com/nightroman/Invoke-Build
+#>
+
+param(
+	[ValidateSet('Debug', 'Release')]
+	[string]$Configuration = 'Release'
+)
+
+# Synopsis: Build project.
+task build {
+	exec { dotnet build -c $Configuration }
+}
+
+# Synopsis: Remove files.
+task clean {
+	remove bin, obj
+}
+
+# Synopsis: Default task.
+task . build
